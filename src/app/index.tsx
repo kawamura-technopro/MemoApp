b3cd39d5d59bbd0887fc4aa1d6ff2c4c35c89f02
kawamura <146682862+kawamura-technopro@users.noreply.github.com>
2023-10-24 11:07:50 +0900

    Header Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,12 +3,14 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
+
       <View>
 
         <View>
@@ -42,6 +44,7 @@ const Index = (): JSX.Element => {
         </View>
 
       </View>
+
       <View>
         <Text>＋</Text>
       </View>
@@ -52,10 +55,40 @@ const Index = (): JSX.Element => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center', // 縦方向
-    alignItems: 'center' // 横方向
+    flex: 1, // 親コンポーネントの範囲とする
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
+    alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255,255,255,0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
+
+/*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, // 親コンポーネントの範囲とする
+    justifyContent: 'center', // 縦方向の中央揃え
+    alignItems: 'center' // 横方向の中央揃え
+  }
+})
+*/
 
 export default Index
