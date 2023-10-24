42c96533a61c8894794374abdbdb1da71b7367ce
kawamura <146682862+kawamura-technopro@users.noreply.github.com>
2023-10-24 13:35:45 +0900

    UI Compornent

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,54 +1,21 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+
+import Header from '../components/Header'
+import MemoListItem from '../components/MemoLIstItem'
+import CircleButton from '../components/CircleButton'
 
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-
-      <View style={styles.header}>
-        <View style={styles.headerInner}>
-          <Text style={styles.headerTitle}>Memo App</Text>
-          <Text style={styles.headerRight}>ログアウト</Text>
-        </View>
-      </View>
+      <Header />
 
       <View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        <View style={styles.memoListItem}>
-          <View>
-            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-            <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
       </View>
 
-      <View style={styles.circleButon}>
-        <Text style={styles.circleButonLabel}>＋</Text>
-      </View>
-
+      <CircleButton>＋</CircleButton>
     </View>
   )
 }
@@ -57,70 +24,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1, // 親コンポーネントの範囲とする
     backgroundColor: '#ffffff'
-  },
-  header: {
-    backgroundColor: '#467FD3',
-    height: 104,
-    justifyContent: 'flex-end',
-  },
-  headerInner: {
-    alignItems: 'center'
-  },
-  headerRight: {
-    position: 'absolute',
-    right: 16,
-    bottom: 16,
-    color: 'rgba(255,255,255,0.7)'
-  },
-  headerTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    fontWeight: 'bold',
-    color: '#ffffff'
-  },
-  memoListItem: {
-    backgroundColor: '#ffffff',
-    flexDirection: 'row', // 本来は縦方向のものを横方向にする
-    justifyContent: 'space-between',
-    paddingVertical: 16,
-    paddingHorizontal: 19,
-    alignItems: 'center', // flexDirection: 'row'で横方向になっているため、alignItemsは横方向から縦方向の指定になる
-    borderBottomWidth: 1,
-    borderColor: 'rgba(0,0,0,0.15)'
-  },
-  memoListItemTitle: {
-    fontSize: 16,
-    lineHeight: 32
-  },
-  memoListItemDate: {
-    fontSize: 12,
-    lineHeight: 16,
-    color: '#848484'
-  },
-  circleButon: {
-    width: 64,
-    height: 64,
-    borderRadius: 32,
-    backgroundColor: '#467FD3',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    right: 40,
-    bottom: 40,
-    // 下4つはiOSのみに適用できるもの
-    shadowColor: '#000000',
-    shadowOpacity: 0.25,
-    shadowRadius: 8,
-    shadowOffset: { width: 0, height: 8 },
-    // 下のelevationはAndroidのみ。
-    elevation: 8 // ボタンの高さを8の位置にあげて影が出るという認識
-  },
-  circleButonLabel: {
-    color: '#ffffff',
-    fontSize: 40,
-    lineHeight: 52
   }
+
 })
 
 /*
