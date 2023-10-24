4bd1e5ce1da788506e35780114a7849f0fdfd4ab
kawamura <146682862+kawamura-technopro@users.noreply.github.com>
2023-10-24 11:57:08 +0900

    Buton Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -45,8 +45,8 @@ const Index = (): JSX.Element => {
 
       </View>
 
-      <View>
-        <Text>＋</Text>
+      <View style={styles.circleButon}>
+        <Text style={styles.circleButonLabel}>＋</Text>
       </View>
 
     </View>
@@ -97,6 +97,29 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 16,
     color: '#848484'
+  },
+  circleButon: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: '#467FD3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    // 下4つはiOSのみに適用できるもの
+    shadowColor: '#000000',
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    // 下のelevationはAndroidのみ。
+    elevation: 8 // ボタンの高さを8の位置にあげて影が出るという認識
+  },
+  circleButonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 52
   }
 })
 
